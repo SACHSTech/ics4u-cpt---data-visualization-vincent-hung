84609124387b65d7c79115a6040de6edb1032e88
VincentHung <vincent.hung21@ycdsbk12.ca>
2021-01-19 01:26:39 -0500

    fxml tests

---
+++ src/CPT/Controller/testController.java
@@ -0,0 +1,5 @@
+package CPT.Controller;
+
+public class testController {
+    
+}
--- src/CPT/Main.java
+++ src/CPT/Main.java
@@ -1,32 +1,21 @@
 package CPT;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-    Button startButton;
-    Stage window;
-    
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/Testing.fxml"));
         primaryStage.setTitle("World Happiness Ranking");
-
-        startButton = new Button();
-        startButton.setText("Happiness");
-        StackPane layout = new StackPane();
-        layout.getChildren().add(startButton);
-
-        Scene scene = new Scene(layout, 500, 500);
-        primaryStage.setScene(scene);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
 
     }
