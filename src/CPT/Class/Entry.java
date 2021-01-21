df88c9785b90a48613baf917d571d2c6a4eee061
VincentHung <vincent.hung21@ycdsbk12.ca>
2021-01-21 14:27:34 -0500

    Tentative Entry Class Finished (comments todo)

--- src/CPT/Class/Entry.java
+++ src/CPT/Class/Entry.java
@@ -1,44 +1,118 @@
 package CPT.Class;
 
-public class Entry {
-    private String strCountry;
-    private double dblScore;
-    private double dblSocial;
-    private double dblLifeExpectancy;
-    private double dblFreedom;
-    private double dblGenerosity;
-    private double dblCorruption;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
-    public Entry(String givenCountry, double givenScore, double givenSocial, double givenExpectancy, double givenFreedom, double givenGenerosity, double givenCorruption){
-        this.strCountry = givenCountry;
-        this.dblScore = givenScore;
-        this.dblSocial = givenSocial;
-        this.dblLifeExpectancy = givenExpectancy;
-        this.dblFreedom = givenFreedom;
-        this.dblGenerosity = givenGenerosity;
-        this.dblCorruption = givenCorruption;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class Entry {
+    private StringProperty strCountry;
+    private DoubleProperty dblScore;
+    private DoubleProperty dblSocial;
+    private DoubleProperty dblLifeExpectancy;
+    private DoubleProperty dblFreedom;
+    private DoubleProperty dblGenerosity;
+    private DoubleProperty dblCorruption;
+
+    public Entry(String givenCountry, double givenScore, double givenSocial, double givenExpectancy,
+            double givenFreedom, double givenGenerosity, double givenCorruption) {
+        this.strCountry = new SimpleStringProperty(givenCountry);
+        this.dblScore = new SimpleDoubleProperty(givenScore);
+        this.dblSocial = new SimpleDoubleProperty(givenSocial);
+        this.dblLifeExpectancy = new SimpleDoubleProperty(givenExpectancy);
+        this.dblFreedom = new SimpleDoubleProperty(givenFreedom);
+        this.dblGenerosity = new SimpleDoubleProperty(givenGenerosity);
+        this.dblCorruption = new SimpleDoubleProperty(givenCorruption);
 
     }
-    
-    public String getCountry() {
+
+    public StringProperty getCountry() {
         return strCountry;
     }
-    public double getScore() {
+
+    public DoubleProperty getScore() {
         return dblScore;
     }
-    public double getSocial() {
+
+    public DoubleProperty getSocial() {
         return dblSocial;
     }
-    public double getExpectancy() {
+
+    public DoubleProperty getExpectancy() {
         return dblLifeExpectancy;
     }
-    public double getFreedom() {
+
+    public DoubleProperty getFreedom() {
         return dblFreedom;
     }
-    public double getGenerosity() {
+
+    public DoubleProperty getGenerosity() {
         return dblGenerosity;
     }
-    public double getCorruption() {
+
+    public DoubleProperty getCorruption() {
         return dblCorruption;
+    }
+
+    public void setCountry(String strchange) {
+        this.strCountry = new SimpleStringProperty(strchange);
+    }
+
+    public void setScore(Double strchange) {
+        this.dblScore = new SimpleDoubleProperty(strchange);
+    }
+
+    public void setSocial(Double strchange) {
+        this.dblSocial = new SimpleDoubleProperty(strchange);
+    }
+
+    public void setExpectancy(Double strchange) {
+        this.dblLifeExpectancy = new SimpleDoubleProperty(strchange);
+    }
+
+    public void setFreedom(Double strchange) {
+        this.dblFreedom = new SimpleDoubleProperty(strchange);
+    }
+
+    public void setGenerosity(Double strchange) {
+        this.dblGenerosity = new SimpleDoubleProperty(strchange);
+    }
+
+    public void setCorruption(Double strchange) {
+        this.dblCorruption = new SimpleDoubleProperty(strchange);
+    }
+
+    public ArrayList<Entry> loadEntries(String filename) {
+            ArrayList<Entry> EntryList = new ArrayList<>();
+            String strread;
+        try {
+            BufferedReader fr = new BufferedReader(new FileReader("CPT/" + filename + ".csv"));
+            while ((strread = fr.readLine()) != null) {
+                String[] EntrySplit = strread.split(",");
+
+                Entry loader = new Entry(strread, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0);
+
+                loader.setCountry(EntrySplit[0]);
+                loader.setScore(Double.parseDouble(EntrySplit[1]));
+                loader.setSocial(Double.parseDouble(EntrySplit[2]));
+                loader.setExpectancy(Double.parseDouble(EntrySplit[3]));
+                loader.setFreedom(Double.parseDouble(EntrySplit[4]));
+                loader.setGenerosity(Double.parseDouble(EntrySplit[5]));
+                loader.setCorruption(Double.parseDouble(EntrySplit[6]));
+
+                EntryList.add(loader);
+            }
+                fr.close();
+        } catch (IOException e) {
+            //Catch Statement
+            e.printStackTrace();
+            System.out.println("Error at Entry.loadEntries");
+        }
+    return EntryList;
     }
 }
