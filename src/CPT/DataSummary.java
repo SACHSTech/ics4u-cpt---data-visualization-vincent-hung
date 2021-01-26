3c16d5035f51a87ef85bbeccb64b9cab32bde84b
VincentHung <vincent.hung21@ycdsbk12.ca>
2021-01-26 16:04:45 -0500

    Green Test Tabpane

---
+++ src/CPT/DataSummary.java
@@ -0,0 +1,11 @@
+package CPT;
+
+import java.util.ArrayList;
+
+import javafx.stage.Stage;
+
+public class DataSummary {
+    public static void dataSummaryScreen(Stage primaryStage, ArrayList<Entry> EntryList) {
+        primaryStage.setWidth(700);
+    }
+}
--- src/CPT/Main.java
+++ src/CPT/Main.java
@@ -3,32 +3,28 @@ package CPT;
 import java.util.ArrayList;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToolBar;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 public class Main extends Application{
+
+    private static Tab hometab;
+
     public static void main(String[] args) {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //Startup Loading
+        // Startup Loading
         primaryStage.setWidth(700);
         primaryStage.setHeight(700);
         primaryStage.setResizable(false);
@@ -42,28 +38,19 @@ public class Main extends Application{
     public static void CentralHub(Stage primaryStage, ArrayList<Entry> EntryList) {
         primaryStage.setWidth(700);
         primaryStage.setHeight(700);
-        BorderPane basePane = new BorderPane();
-        basePane.setPadding(new Insets(10,10,10,10));
 
-        //Top Bar
+        // Top Bar
         TabPane tabPane = new TabPane();
-        Button homebutton = new Button("Home");
-        Button searchbutton = new Button("Search");
-        Button graphsbutton = new Button("Graphs");
-        Button summarybutton = new Button("Summary");
-        tophBox.getChildren().addAll(homebutton, searchbutton, graphsbutton, summarybutton);
-        HBox.setMargin(homebutton, new Insets(10, 10, 10, 10));
-        HBox.setMargin(searchbutton, new Insets(10, 10, 10, 10));
-        HBox.setMargin(graphsbutton, new Insets(10, 10, 10, 10));
-        HBox.setMargin(summarybutton, new Insets(10, 10, 10, 10));
-        basePane.setTop(tophBox);
-
-        //Left Bar
-        //Center
-        ImageView cover = new ImageView(new Image("src/CPT.cover.jpg"));
-        basePane.setCenter(cover);
+        hometab = new Tab();
+        tabPane.getTabs().add(hometab);
+        final VBox vbox = new VBox();
+        vbox.setSpacing(10);
+        vbox.setTranslateX(10);
+        vbox.setTranslateY(10);
+        hometab.setText("Home");
+        hometab.setTooltip(new Tooltip("Tab 1 Tooltip"));
         //Show Menu
-        primaryStage.setScene(new Scene(basePane));
+        primaryStage.setScene(new Scene(tabPane));
         primaryStage.show();
     }
 
